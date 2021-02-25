747e2e7f51ce0a0d20719e2a4b9bf5569b835f46
Norhani A. Ayaon <ayaon.na27@s.msumain.edu.ph>
2021-02-25 14:56:24 +0800

    initial commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var db = level('./leveldb');
 
 // Issue #3 
 
-var status = ['Applying'];
+var status = ['Applying', 'Under Interview'];
 acceptStudent(' 201811827 ', ' Norhani A. Ayaon ',' 21 ', ' Marawi City ');
 function acceptStudent(id, fullName, age, address){
     console.log("   ------ Student Information ------  ");
@@ -13,7 +13,18 @@ function acceptStudent(id, fullName, age, address){
         
     })
     db.get(id, function(err, value){
-        console.log(value,'Student: ', status[0]); 
+        console.log(value,'\n Status: ', status[0]); 
     })
-
+    scheduleInterview(id, 'March 02, 2021');
 }
+
+// Issue #4
+async function scheduleInterview(id, scheduleDate){
+    await db.get(id, function(err, value){
+        console.log('\n')
+        console.log(value);
+        console.log(' Interview date:  ' + scheduleDate);
+        console.log(' Status: ' + status[1])
+    })
+}
+//OUTPUT = ['201811827', 'Norhani A. Ayaon', 22, 'Marawi City']
