1ab39b6f3b34c5e1ea8c064524c8b008c7e45f81
Norhani A. Ayaon <ayaon.na27@s.msumain.edu.ph>
2021-02-25 12:08:01 +0800

    accept student

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 var level = require('level');
 
 var db = level('./leveldb');
+
+
+// Issue #3 
+
+var status = ['Applying'];
+acceptStudent(' 201811827 ', ' Norhani A. Ayaon ',' 21 ', ' Marawi City ');
+function acceptStudent(id, fullName, age, address){
+    console.log("   ------ Student Information ------  ");
+    db.put(id, [id, fullName , age , address], function(err){
+        
+    })
+    db.get(id, function(err, value){
+        console.log(value, status[0]); 
+    })
+
+}
