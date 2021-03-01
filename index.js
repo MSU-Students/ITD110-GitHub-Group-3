f20e1afdfe0dab13b6ce43cc13c0a2abf1438db4
Norjehan M. Alango <alango.nm88@s.msumain.edu.ph>
2021-03-01 15:07:27 +0800

    connectToDatabase

--- index.js
+++ index.js
@@ -1,30 +1,6 @@
-var level = require('level');
+const level = require('level');
+const db = connectToDatabase('./leveldb');
 
-var db = level('./leveldb');
-
-
-// Issue #3 
-
-var status = ['Applying', 'Under Interview'];
-acceptStudent(' 201811827 ', ' Norhani A. Ayaon ',' 21 ', ' Marawi City ');
-function acceptStudent(id, fullName, age, address){
-    console.log("   ------ Student Information ------  ");
-    db.put(id, [id, fullName , age , address], function(err){
-        
-    })
-    db.get(id, function(err, value){
-        console.log(value,'\n Status: ', status[0]); 
-    })
-    scheduleInterview(id, 'March 02, 2021');
+function connectToDatabase(db){
+    return level(db, { valueEncoding: 'json' });
 }
-
-// Issue #4
-async function scheduleInterview(id, scheduleDate){
-    await db.get(id, function(err, value){
-        console.log('\n')
-        console.log(value);
-        console.log(' Interview date:  ' + scheduleDate);
-        console.log(' Status: ' + status[1])
-    })
-}
-//OUTPUT = ['201811827', 'Norhani A. Ayaon', 22, 'Marawi City']
