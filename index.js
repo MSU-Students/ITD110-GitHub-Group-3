807c4b6cf694d23b99d1e8a9a7766bc46ac14978
Lucman Abdulrachman <fencim@gmail.com>
2021-03-01 17:29:39 +0800

    set ID on interview

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var status = ['Applying', 'Under Interview', 'Exam Pending', 'Admitted', 'Probat
 (async function() {
     await acceptStudent('201811827', 'Norhani A. Ayaon', 22, 'Marawi City');
     var ScheduleDate = 'Februaryd 1, 2021';
-    await scheduleInterview(id, ScheduleDate);
+    await scheduleInterview('201811827', ScheduleDate);
 }());
 
 function connectToDatabase(dbName){
